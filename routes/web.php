019b07bc7bcf31f300811da7871161792e5614ac
coderkhayrul <coderkhayrul@gmail.com>
2021-11-29 01:14:48 +0600

    Admin Panel Add

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,6 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/user/logout', [HomeController::class, 'logout'])->name('user.logout');
 Route::get('/change-password', [ChangePasswordController::class, 'index'])->name('password.change');
